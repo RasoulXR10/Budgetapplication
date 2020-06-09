222bc7d69dbc9bcd627d605af362def9eaa99d73
RasoulXR10 <48275825+RasoulXR10@users.noreply.github.com>
2020-06-09 05:22:52 +0430

    Basic SetUp

---
+++ BudgetApp/budget/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projects = [
+    {
+        'author': 'rasoul',
+        'title': 'Budget Project 1',
+        'description': 'First project description',
+        'date_created': 'Jun 9, 2020'
+    },
+    {
+        'author': 'pouria',
+        'title': 'Budget Project 2',
+        'description': 'Second project description',
+        'date_created': 'Jun 9, 2020'
+    },
+]
+
+
+def home(request):
+    content = {
+        'projects': projects,
+    }
+    return render(request, 'budget/home.html', content)
+
+
+def profile(request):
+    return render(request, 'budget/profile.html', {'title': 'Profile'})
